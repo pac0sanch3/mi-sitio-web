48d5676af65e1d5b2f55b03fa8b1ef8b63d811eb
pac0sanch3 <noriegatrujolloj123@gmail.com>
2023-06-13 13:03:43 -0500

    subiendo los cambiosdepues del error 403

--- js/transitions.js
+++ js/transitions.js
@@ -1,14 +1,19 @@
-let movimiento=document.getElementById("moviento")
-let content=document.getAnimations("content")
+ 
+
+let movimiento=document.getElementById("movimiento");
+let content=document.getElementById("content");
 
 movimiento.addEventListener("click", function(){
-    if (content.classList.contains("box_4_1")) {
-        content.classList.add("box_5");
-        content.classList.remove("box_4_1");
-
+    if(content.classList.contains("contenedor-1")){
+        content.classList.add("contenedor-2")
+        content.classList.remove("contenedor-1")
+        movimiento.classList.add("main-box-on");
+        movimiento.classList.remove("main-box-off");
     }
     else{
-        content.classList.remove("box_4_1");
-        content.classList.add("box_5");
+        content.classList.remove("contenedor-2")
+        content.classList.add("contenedor-1")
+        movimiento.classList.remove("main-box-on");
+        movimiento.classList.add("main-box-off");
     }
 })
