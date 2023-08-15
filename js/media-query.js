b9f9318bca05fdcf66b329fd65d8a42bfa2c6620
coronadojulianm <juliancoronadom@gmail.com>
2023-08-15 16:41:36 -0500

    starbucks

---
+++ js/media-query.js
@@ -0,0 +1,14 @@
+
+let logohamburguer=document.getElementById("logohamburguer")
+let mainMenu=document.getElementById("mainMenu");
+
+logohamburguer.addEventListener("click",function(){
+    if (mainMenu.classList.contains("main-menu")) {
+        mainMenu.classList.add("main-menu-block");
+        mainMenu.classList.remove("main-menu"); 
+    }
+    else{
+        mainMenu.classList.remove("main-menu-block");
+        mainMenu.classList.add("main-menu");
+    }
+})
