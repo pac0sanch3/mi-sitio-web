7995a852cc849fb2411f0b9da3d8713527fb567d
pac0sanch3 <noriegatrujolloj123@gmail.com>
2023-10-17 15:35:04 -0500

    f

--- js/carrito.js
+++ js/carrito.js
@@ -1,16 +1,34 @@
-let boxPlus = document.getElementById("boxPlus")
-let boxUnidades = document.getElementById("boxUnidades")
-let boxMinus = document.getElementById("boxMinus")
+let boxPlus = document.getElementById("boxPlus");
+let boxUnidades = document.getElementById("boxUnidades");
+let boxMinus = document.getElementById("boxMinus");
+let subtotal = document.getElementById("subTotal");
+const valorUnidad=67000;
+const valorUnidadStr=Intl.NumberFormat("de-DE").format(valorUnidad);
+document.getElementById("precioUnidad").innerHTML="$" + valorUnidadStr;
 
 boxPlus.addEventListener("click", function() {
-    let auxUnidades = parseInt(boxUnidades.innerHTML)
+    let auxUnidades = parseInt(boxUnidades.innerHTML);
     if (auxUnidades < 10 ) {
-        boxUnidades.innerHTML = (auxUnidades + 1).toString()
+        boxUnidades.innerHTML = (auxUnidades + 1).toString();
+        let valorsubTotal=parseInt(boxUnidades.innerHTML) * valorUnidad;
+        subtotal.innerHTML=Intl.NumberFormat("de-DE").format(valorsubTotal);
+        if(auxUnidades.innerHTML==1){
+            document.getElementById("canProductos").innerHTML=boxUnidades.innerHTML + " producto";
+        }else{
+            document.getElementById("canProductos").innerHTML=boxUnidades.innerHTML + " productos";
+        }
     }
 })
 boxMinus.addEventListener("click", function() {
-    let auxUnidades = parseInt(boxUnidades.innerHTML)
+    let auxUnidades = parseInt(boxUnidades.innerHTML);
     if (auxUnidades > 0 ) {
-        boxUnidades.innerHTML = (auxUnidades - 1).toString()
+        boxUnidades.innerHTML = (auxUnidades - 1).toString();
+        let valorsubTotal=parseInt(boxUnidades.innerHTML) * valorUnidad;
+        subtotal.innerHTML=Intl.NumberFormat("de-DE").format(valorsubTotal);
+        if(auxUnidades.innerHTML==1){
+            document.getElementById("canProductos").innerHTML=boxUnidades.innerHTML + "producto";
+        }else{
+            document.getElementById("canProductos").innerHTML=boxUnidades.innerHTML + "productos";
+        }
     }
 })
