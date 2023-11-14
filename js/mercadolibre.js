a2a1f935674a0625cad4ade8651f1af3661b04d8
pac0sanch3 <noriegatrujolloj123@gmail.com>
2023-11-14 13:38:59 -0500

    problema resuelto

--- js/mercadolibre.js
+++ js/mercadolibre.js
@@ -37,18 +37,18 @@ marcaProducto3.appendChild(nodoMarcaProducto5)
 
 let marcaProducto4=document.createElement("label")
 infoproducto.appendChild(marcaProducto4)
-let nodoMarcaProducto3=document.createTextNode(" 12.327 km ")
+let nodoMarcaProducto3=document.createTextNode("2023 · 12.327 km · bolivar-bogota D.C")
 marcaProducto4.appendChild(nodoMarcaProducto3)
 
-let marcaProducto2=document.createElement("label")
+/* let marcaProducto2=document.createElement("label")
 infoproducto.appendChild(marcaProducto2)
-let nodoMarcaProducto2=document.createTextNode(" 2023 ")
-marcaProducto2.appendChild(nodoMarcaProducto2)
-
+let nodoMarcaProducto2=document.createTextNode(" 2023 ·")
+marcaProducto2.appendChild(nodoMarcaProducto2) */
+/* 
 let marcaProducto5=document.createElement("label")
 infoproducto.appendChild(marcaProducto5)
 let nodoMarcaProducto4=document.createTextNode(" bolivar-bogota D.C")
-marcaProducto5.appendChild(nodoMarcaProducto4)
+marcaProducto5.appendChild(nodoMarcaProducto4) */
 
 
 
