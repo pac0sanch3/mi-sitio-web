92dc598f5975bdde0ab60a0b03c3e70d0223b120
jhon-frand <frandlebaza@gmail.com>
2023-12-12 16:37:01 -0500

    estrellas

--- js/amazon.js
+++ js/amazon.js
@@ -4,35 +4,30 @@ let calificacion3 = document.getElementById("calificacion3")
 let calificacion4 = document.getElementById("calificacion4")
 let calificacion5 = document.getElementById("calificacion5")
 let btnGraficar = document.getElementById("btnGraficar")
+let porcentaje = document.getElementById("PorcentajeCalificacion1")
+let boxTextoCalificaion=document.getElementById("boxTextoCalificacion")
 
 btnGraficar.addEventListener("click", function() {
-    let sumaCalificacion = Number(calificacion1.value) + Number(calificacion2.value) + Number(calificacion3.value) + Number(calificacion4.value) + Number(calificacion5.value)
-/*     alert(sumaCalificacion) */
+    let sumaCalificacion =  Number(calificacion1.value) + 
+                            Number(calificacion2.value) + 
+                            Number(calificacion3.value) + 
+                            Number(calificacion4.value) + 
+                            Number(calificacion5.value)
+                            console.log(sumaCalificacion)
 
-    let promCalificacion1 = calificacion1.value * 100  / sumaCalificacion
-    let promCalificacion2 = calificacion2.value * 100  / sumaCalificacion
-    let promCalificacion3 = calificacion3.value * 100  / sumaCalificacion
-    let promCalificacion4 = calificacion4.value * 100  / sumaCalificacion
-    let promCalificacion5 = calificacion5.value * 100  / sumaCalificacion
-    alert(promCalificacion1)
-/*     calificacion1.value
-    calificacion2.value
-    calificacion3.value
-    calificacion4.value
-    calificacion5.value
+    let promCalificacion1 = ((calificacion1.value * 100 ) / sumaCalificacion)
+    let promCalificacion2 = ((calificacion2.value * 100)  / sumaCalificacion)
+    let promCalificacion3 = ((calificacion3.value * 100)  / sumaCalificacion)
+    let promCalificacion4 = ((calificacion4.value * 100)  / sumaCalificacion)
+    let promCalificacion5 = ((calificacion5.value * 100)  / sumaCalificacion)
+    console.log(promCalificacion1.toFixed(0))
+    porcentaje.style.width=(promCalificacion1 * 2) +"px"
 
-    let sumar1 = (calificacion1.value/100)
-    alert(sumar1)
+    let spanPorcentaje = document.createElement("span")
+    boxTextoCalificaion.appendChild(spanPorcentaje)
+    spanPorcentaje.innerHTML=promCalificacion1.toFixed(0)+"%"
+    spanPorcentaje.setAttribute('class', 'span-porcentaje');
 
-    let sumar2 = (calificacion2.value/100)
-    alert(sumar2)
 
-    let sumar3 = (calificacion3.value/100)
-    alert(sumar3)
 
-    let sumar4 = (calificacion4.value/100)
-    alert(sumar4)
-
-    let sumar5 = (calificacion5.value/100)
-    alert(sumar5) */
 })
